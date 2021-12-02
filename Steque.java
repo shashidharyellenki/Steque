b5e9ae3a7490e73e1e6304fd0dd074881a3a03fb
shashidharyellenki <yellenkishashidhar@msitprogram.net>
2021-12-02 19:26:27 +0530

    push

--- Steque.java
+++ Steque.java
@@ -71,7 +71,12 @@ public class Steque<Item> implements Iterable<Item> {
      */
     //time complexity:O(1), space complexity: O(n)
     public void push(Item item) {
-       
+        if(item == null) throw new IllegalArgumentException();
+        Node oldfirst = first;
+        first = new Node();
+        first.item = item;
+        first.next = oldfirst;
+        if(last == null) last=first;
 
     }
     
